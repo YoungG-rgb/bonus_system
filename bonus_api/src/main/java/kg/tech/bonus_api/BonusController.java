d926f7d270979c979dab2fd55fadd9efeb2dddbb
Zhalaldin Toichubaev <zhtoichubaev@mail.ru>
2025-03-20 01:53:12 +0600

    add jaeger

--- bonus_api/src/main/java/kg/tech/bonus_api/BonusController.java
+++ bonus_api/src/main/java/kg/tech/bonus_api/BonusController.java
@@ -1,16 +1,19 @@
 package kg.tech.bonus_api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/bonuses")
 public class BonusController {
 
     @GetMapping("/{id}")
     public String getById(@PathVariable("id") Long id) {
+        log.info("getById id {}", id);
         return "request: getById " + id;
     }
 }
