46745bdc3173693c7a91efd15a5f7a1248af35b3
Zhalaldin Toichubaev <zhtoichubaev@mail.ru>
2025-03-20 00:59:26 +0600

    add bonus-referral-system

---
+++ bonus_referral/src/main/java/kg/tech/bonus_referral/BonusReferralApplication.java
@@ -0,0 +1,13 @@
+package kg.tech.bonus_referral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusReferralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonusReferralApplication.class, args);
+    }
+
+}
